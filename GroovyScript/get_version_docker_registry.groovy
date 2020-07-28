aea5c78293135936edc6dff720c9fa09160d6168
anetmex <net.amex@gmail.com>
2020-07-28 19:12:13 +0300

    c

--- GroovyScript/get_version_docker_registry.groovy
+++ GroovyScript/get_version_docker_registry.groovy
@@ -2,7 +2,7 @@
 import groovy.json.JsonSlurper
 try {
     List<String> artifacts = new ArrayList<String>()
-    def artifactsUrl = "http://3.8.97.63:5000/v2/simple-rest-app_1.0/tags/list"
+    def artifactsUrl = "http://10.0.1.117:443/v2/simple-rest-app_1.0/tags/list"
     def artifactsObjectRaw = ["curl", "-s", "-H", "accept: application/json", "-k", "--url", "${artifactsUrl}"].execute().text
     def jsonSlurper = new JsonSlurper()
     def artifactsJsonObject = jsonSlurper.parseText(artifactsObjectRaw)
